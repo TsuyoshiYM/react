0709d31e45fea3cc7902bb93ad70f4b9d1b8c9c1
Frank Yashimura <51141488+TsuyoshiYM@users.noreply.github.com>
2025-07-31 16:58:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 // Configuración de CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'https://sprightly-tiramisu-62a3eb.netlify.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
